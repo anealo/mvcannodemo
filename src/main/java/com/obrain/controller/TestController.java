69ad4b02d501b1055b4fe9e02d0f7f691e7f628b
anealo <order491key@sina.com>
2019-10-27 22:41:35 +0800

    新增注解@MyRequestParam； 修复请求参数中有多个String参数时，在调用时参数值被覆盖的bug；

--- src/main/java/com/obrain/controller/TestController.java
+++ src/main/java/com/obrain/controller/TestController.java
@@ -1,10 +1,11 @@
 package com.obrain.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
 
 import com.obrain.annotation.MyAutowired;
 import com.obrain.annotation.MyController;
 import com.obrain.annotation.MyRequestMapping;
+import com.obrain.annotation.MyRequestParam;
 import com.obrain.service.TestService;
 
 @MyController
@@ -20,8 +21,41 @@ public class TestController {
 		return "Test springMVC:"+testService.test();
 	}
 	
-	@MyRequestMapping("/index2")
-	public String index2() {
-		return "Test springMVC:"+testService.test();
+	@MyRequestMapping("/user")
+	public String index2(@MyRequestParam String name,@MyRequestParam String age) {
+		return "姓名：:"+name+",年龄："+age;
+	}
+	
+	@MyRequestMapping("/info")
+	public String index3(@MyRequestParam String name,@MyRequestParam String age,@MyRequestParam HttpServletRequest req) {
+		System.out.println("ContextPath:"+ req.getContextPath()+"\r\n"+
+		"CharacterEncodin:"+ req.getCharacterEncoding()+"\r\n"+
+//		"AsyncContext:"+req.getAsyncContext()+"\r\n"+
+		"ContentLength:"+req.getContentLength()+"\r\n"+
+		"ContentType:"+req.getContentType()+"\r\n"+
+//		"HttpServletMapping:"+req.getHttpServletMapping()+"\r\n"+   //java.lang.NoSuchMethodError
+		"LocalAddr:"+req.getLocalAddr()+"\r\n"+
+		"Locale:"+req.getLocale()+"\r\n"+
+		"LocalName:"+req.getLocalName()+"\r\n"+
+		"Method:"+req.getMethod()+"\r\n"+
+		"ParameterMap:"+req.getParameterMap()+"\r\n"+
+		"ParameterNames:"+req.getParameterNames()+"\r\n"+
+		"HeaderNames:"+req.getHeaderNames()+"\r\n"+
+		"PathInfo:"+req.getPathInfo()+"\r\n"+
+		"QueryString:"+req.getQueryString()+"\r\n"+
+		"RemoteAddr:"+req.getRemoteAddr()+"\r\n"+
+		"RemoteHost:"+req.getRemoteHost()+"\r\n"+
+		"RemotePort:"+req.getRemotePort()+"\r\n"+
+		"RequestedSessionId:"+req.getRequestedSessionId()+"\r\n"+
+		"RequestURI:"+req.getRequestURI()+"\r\n"+
+		"RequestURL:"+req.getRequestURL()+"\r\n"+
+		"Scheme:"+req.getScheme()+"\r\n"+
+		"ServerName:"+req.getServerName()+"\r\n"+
+		"ServerPort:"+req.getServerPort()+"\r\n"+
+		"ServletContext:"+req.getServletContext()+"\r\n"+
+		"ServletPath:"+req.getServletPath()+"\r\n"+
+		"Session:"+req.getSession()
+		);
+		return "姓名：:"+name+",年龄："+age;
 	}
 }
--- src/main/java/com/obrain/servlet/MyDispatcherServlet.java
+++ src/main/java/com/obrain/servlet/MyDispatcherServlet.java
@@ -5,17 +5,25 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.obrain.annotation.MyAutowired;
 import com.obrain.annotation.MyController;
 import com.obrain.annotation.MyRequestMapping;
@@ -26,9 +34,11 @@ import com.obrain.annotation.MyService;
  */
 public class MyDispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	// 包扫描得到的所有类名，如：com.obrain.controller.TestController.class
 	List<String> clazzNames = new ArrayList<String>();
+	// 类映射路径和类实例化的映射关系，如/test对应TestController，/testService对应TestService
 	Map<String, Object> beans = new HashMap<String, Object>();
+	// 路径和方法的映射关系，如（在TestController中）/test/index对应index()
 	Map<String, Object> handlerMethod = new HashMap<String, Object>();
 
 	/**
@@ -49,60 +59,61 @@ public class MyDispatcherServlet extends HttpServlet {
 		}
 		// 2.类实例化
 		classInstance();
-		
-		//3.完整自动注入的装入
+
+		// 3.完成整自动注入的装入
 		inject();
-		
-		//4.完成url和controller中method的映射
+
+		// 4.完成url和controller中method的映射
 		handlerMapping();
+
 	}
 
 	private void handlerMapping() {
-		if(beans.isEmpty()) {
+		if (beans.isEmpty()) {
 			System.out.println("没有实例化的类");
 			return;
 		}
-		for(Map.Entry<String,Object> entry : beans.entrySet()) {
-			//从集合中获取类实例
+		for (Map.Entry<String, Object> entry : beans.entrySet()) {
+			// 从集合中获取类实例
 			Object instance = entry.getValue();
-			if(instance.getClass().isAnnotationPresent(MyController.class)) {
+			if (instance.getClass().isAnnotationPresent(MyController.class)) {
 				MyRequestMapping requestMapping = instance.getClass().getAnnotation(MyRequestMapping.class);
-				//获取类上的requestMapping中个 定义的路径值=>@RequestMapping("/test")
+				// 获取类上的requestMapping中个 定义的路径值=>@RequestMapping("/test")
 				String path = requestMapping.value();
-				
-				//获取类成员属性
+
+				// 获取类成员属性
 				Method[] methods = instance.getClass().getDeclaredMethods();
-				for(Method method : methods) {
-					//获取方法上的yRequestMapping("/index")
+				for (Method method : methods) {
+					// 获取方法上的yRequestMapping("/index")
 					MyRequestMapping methodMapping = method.getAnnotation(MyRequestMapping.class);
 					String value = methodMapping.value();
-					//类上的路径+方法上的路径
-					handlerMethod.put(path+value, method);
-					
+					// 类上的路径+方法上的路径
+					handlerMethod.put(path + value, method);
+
 				}
 			}
 		}
 	}
 
 	private void inject() {
-		if(beans.isEmpty()) {
+		if (beans.isEmpty()) {
 			System.out.println("没有实例化的类");
 			return;
 		}
-		for(Map.Entry<String,Object> entry : beans.entrySet()) {
-			//从集合中获取类实例
+		for (Map.Entry<String, Object> entry : beans.entrySet()) {
+			// 从集合中获取类实例
 			Object instance = entry.getValue();
 			Field[] fields = instance.getClass().getDeclaredFields();
-			for(Field field :fields) {
-				//属性上是否有批注@MyAutowired
-				if(field.isAnnotationPresent(MyAutowired.class)) {
+			for (Field field : fields) {
+				// 属性上是否有批注@MyAutowired
+				if (field.isAnnotationPresent(MyAutowired.class)) {
 					MyAutowired autowired = field.getAnnotation(MyAutowired.class);
-					String value  = autowired.value();
+					String value = autowired.value();
 					field.setAccessible(true);
 					try {
-						//最关键的一行，完成依赖注入
+						// 最关键的一行，完成依赖注入
 						field.set(instance, beans.get(value));
-						
+
 					} catch (IllegalArgumentException e) {
 						e.printStackTrace();
 					} catch (IllegalAccessException e) {
@@ -189,19 +200,121 @@ public class MyDispatcherServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		String uri = request.getRequestURI();
 		String contextPath = request.getContextPath();
-		//mvcannodemo/test/index=>/test/index
+		// mvcannodemo/test/index=>/test/index
 		String path = uri.replaceAll(contextPath, "");
 		Method method = (Method) handlerMethod.get(path);
-		Object instance = beans.get("/"+path.split("/")[1]);
+		Object instance = beans.get("/" + path.split("/")[1]);
 		try {
 			if(method==null) {
-				return;
+				response.getWriter().write("404 NOT FOUND!");
+				return ;
 			}
-			Object obj = method.invoke(instance, null);
+			// 获取参数
+			Object[] params = getRequestParam(request, response);
+
+			Object obj = method.invoke(instance, params);
 			System.out.println(obj);
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private Object[] getRequestParam(HttpServletRequest req, HttpServletResponse resp) {
+		String url = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		url = url.replace(contextPath, "").replaceAll("/+", "/");
+
+		Method method = (Method) handlerMethod.get(url);
+		// 获取方法的参数列表
+		Class<?>[] parameterTypes = method.getParameterTypes();
+		Parameter[] methodParams = method.getParameters();
+		// 获取请求的参数
+
+		Map<String, String[]> requestParameterMap = req.getParameterMap();
+
+		// 保存参数值
+
+		Object[] invokeParams = new Object[parameterTypes.length];
+		for(int i = 0; i < methodParams.length; i++) {
+			String methodParamName = methodParams[i].getName();
+			Type methodParamType = methodParams[i].getParameterizedType();
+//			System.out.println("获取的methodParamType："+methodParamType);//class java.lang.String	     interface javax.servlet.http.HttpServletRequest
+//			Type methodParamType1 = methodParams[i].getType();
+//			System.out.println("获取的methodParamType1："+methodParamType1);//class java.lang.String    interface javax.servlet.http.HttpServletRequest
+//			System.out.println("TypeName:"+methodParamType.getTypeName());//java.lang.String
+//			System.out.println("Class:"+methodParamType.getClass());//class java.lang.Class
+//			System.out.println("toString:"+methodParamType.toString());//class java.lang.String
+			if("java.lang.String".equals(methodParamType.getTypeName())) {
+				for (Entry<String, String[]> param : requestParameterMap.entrySet()) {
+					  if(methodParamName.equals(param.getKey())) {
+						  
+						  String value = Arrays.toString(param.getValue()).replaceAll("\\[|\\]","").replaceAll(",\\s", ",");
+						  
+						  invokeParams[i] = value;
+						  break;
+					  }
+				  }
+			  
+			}else if("javax.servlet.http.HttpServletRequest".equals(methodParamType.getTypeName())) {
+				invokeParams[i] = req;
+				continue;
+			}else if("javax.servlet.http.HttpServletResponse".equals(methodParamType.getTypeName())) {
+				invokeParams[i] = resp;
+				continue;
+			}
+			
+		}
+		// 方法的参数列表
+		/*
+		 * for (int i = 0; i < parameterTypes.length; i++) { // 根据参数名称，做某些处理
+		 * 
+		 * String methodParamType = parameterTypes[i].getSimpleName();
+		 * 
+		 * if (methodParamType.equals("HttpServletRequest")) {
+		 * 
+		 * // 参数类型已明确，这边强转类型
+		 * 
+		 * invokeParams[i] = req;
+		 * 
+		 * continue;
+		 * 
+		 * }
+		 * 
+		 * if (methodParamType.equals("HttpServletResponse")) {
+		 * 
+		 * invokeParams[i] = resp;
+		 * 
+		 * continue;
+		 * 
+		 * }
+		 * 
+		 * if (methodParamType.equals("String")) {
+		 * 
+		 * // JSONArray jSONArray = (JSONArray) JSONArray.toJSON(requestParameterMap);
+		 * // System.out.println("第一次jSONArray:"+jSONArray);
+		 * System.out.println(JSON.toJSONString(requestParameterMap)); String mapStr =
+		 * JSON.toJSONString(requestParameterMap,
+		 * SerializerFeature.BrowserCompatible).replace("\\\\u", "\\u"); // JSONArray
+		 * arr = JSONArray.parseArray(mapStr); // System.out.println("第二次arr:"+arr);
+		 * 
+		 * JSONObject jsonObject = JSON.parseObject(mapStr);
+		 * System.out.println("第三次转为jsonObject："+jsonObject); }
+		 * 
+		 * if (methodParamType.equals("String")) {
+		 * 
+		 * for (Entry<String, String[]> param : requestParameterMap.entrySet()) {
+		 * 
+		 * String value =
+		 * Arrays.toString(param.getValue()).replaceAll("\\[|\\]","").replaceAll(",\\s",
+		 * ",");
+		 * 
+		 * invokeParams[i] = value; if(i <= parameterTypes.length-2) { i++; } }
+		 * 
+		 * }
+		 * 
+		 * }
+		 */
+		return invokeParams;
 	}
 
 }
